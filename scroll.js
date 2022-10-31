98dac38d4a4ae6be8e6d72dff7aa60e64a998ec9
moayadsamy <7404f86580@gmail.com>
2022-10-31 19:43:29 +0200

    Black and White

---
+++ scroll.js
@@ -0,0 +1,33 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let moayad = document.querySelector('.moayad');
+
+onscroll = function(){
+    let value = scrollY;
+    stars.style.left = value + 'px';
+    moon.style.top = value * 3 + 'px';
+    mountains3.style.top = value * 2 + 'px';
+    mountains4.style.top = value * 1.5 + 'px';
+    river.style.top = value + 'px';
+    boat.style.top = value + 'px';
+    boat.style.left = value * 3 + 'px';
+    moayad.style.fontSize = value + 'px';
+    if(scrollY >= 67){
+        moayad.style.fontSize = 67 + 'px';
+        moayad.style.position = "fixed";
+        if(scrollY >= 478){
+        moayad.style.display = "none";
+        }else {
+        moayad.style.display = "block";
+        }
+        if(scrollY >= 127){
+            document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)'
+        }else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001f)'
+        }
+    }
+}
